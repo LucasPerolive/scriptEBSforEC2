edf8edd085b6bcd6432637ff2b30325f9a600506
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2025-01-20 11:02:06 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,26 +7,23 @@ read INSTANCE_ID
 # Obter a lista de discos associados à instância
 DISKS=$(aws ec2 describe-instances \
   --instance-id "$INSTANCE_ID" \
-  --query "Reservations[].Instances[].BlockDeviceMappings[?Ebs.DeleteOnTermination==\`false\`]" \
+  --query "Reservations[].Instances[].BlockDeviceMappings" \
   --output json)
 
-# Verificar se existem discos com DeleteOnTermination desabilitado
-if [ "$DISKS" == "[]" ]; then
-  echo "Todos os discos já possuem a exclusão automática habilitada."
-  exit 0
-fi
-
-# Iterar sobre cada disco encontrado
+# Verificar se existem discos e iterar sobre eles
 echo "$DISKS" | jq -c '.[]' | while read -r disk; do
+  DELETE_ON_TERMINATION=$(echo "$disk" | jq -r '.Ebs.DeleteOnTermination')
   DEVICE_NAME=$(echo "$disk" | jq -r '.DeviceName')
 
-  echo "Habilitando DeleteOnTermination para o disco: $DEVICE_NAME"
+  # Verificar se DeleteOnTermination está desabilitado
+  if [ "$DELETE_ON_TERMINATION" == "false" ]; then
+    echo "Habilitando DeleteOnTermination para o disco: $DEVICE_NAME"
 
-  # Habilitar DeleteOnTermination para o disco atual
-  aws ec2 modify-instance-attribute \
-    --instance-id "$INSTANCE_ID" \
-    --block-device-mappings "[{\"DeviceName\": \"$DEVICE_NAME\", \"Ebs\": {\"DeleteOnTermination\": true}}]"
+    # Habilitar DeleteOnTermination para o disco atual
+    aws ec2 modify-instance-attribute \
+      --instance-id "$INSTANCE_ID" \
+      --block-device-mappings "[{\"DeviceName\": \"$DEVICE_NAME\", \"Ebs\": {\"DeleteOnTermination\": true}}]"
+  fi
 done
 
 echo "Configuração concluída!"
-
